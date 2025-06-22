cb7c4633b81ddbc24306af7c6c0b39dbeb30f4d4
cesar_vargas <cesarluisvargaslezano@gmail.com>
2025-06-22 12:05:22 -0400

    fix: update code for use Guards, and corrected styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'login-app';
+export class AppComponent implements OnInit {
+  constructor(private _router: Router, private _authService: AuthService) {
+  }
+
+  public ngOnInit() {
+    this._router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (!event.urlAfterRedirects.includes('/user-information')) {
+          this._authService._logOut();
+        }
+      }
+    });
+  }
 }
